1452ede0bbb92a20ebc04c6853f3b21de0ea1ce7
Bibash45 <bibashcdry46@gmail.com>
2024-12-14 22:02:46 +0545

    frontend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,29 @@
+import mongoose, { model } from "mongoose";
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
 
 // env variable
+const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 8000;
+
+// exit on mongodb error
+mongoose.connection.on("error", (err) => {
+  logger.error(`Mongodb connection error: ${err}`);
+  process.exit(1);
+});
+
+// mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+//mongodb connection
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    logger.info("Connected to Mongodb.");
+  })
+  .catch((err) => logger.error("Error connecting to Mongodb: ", err));
 
 let server;
 server = app.listen(PORT, () => {
