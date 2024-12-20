c7d0e6d1b2904f3be85977d0f16d838d246d9956
Bibash45 <bibashcdry46@gmail.com>
2024-12-20 20:09:05 +0545

    search user

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,17 @@
+import createHttpError from "http-errors";
+import logger from "../configs/logger.config.js";
+import { searchUsers as searchUserService } from "../services/user.service.js";
+
+export const searchUsers = async (req, res, next) => {
+  try {
+    const keyword = req.query.search;
+    if (!keyword) {
+      logger.error("Please add a search query first");
+      throw createHttpError.BadRequest("Opps....something went wrong");
+    }
+    const users = await searchUserService(keyword);
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./auth.route.js";
 import coversationRoutes from "./conversation.route.js";
+import userRoutes from "./user.route.js";
 import messageRoutes from "./message.route.js";
 
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/conversation", coversationRoutes);
 router.use("/message", messageRoutes);
+router.use("/user", userRoutes);
 
 export default router;
