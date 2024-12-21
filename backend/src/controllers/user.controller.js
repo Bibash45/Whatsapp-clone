1cb80451e78584df4232eedd31ec82f651e68583
Bibash45 <bibashcdry46@gmail.com>
2024-12-21 17:47:45 +0545

    some updates in the backend search

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,12 +4,13 @@ import { searchUsers as searchUserService } from "../services/user.service.js";
 
 export const searchUsers = async (req, res, next) => {
   try {
+    const userId = req.user.userId
     const keyword = req.query.search;
     if (!keyword) {
       logger.error("Please add a search query first");
       throw createHttpError.BadRequest("Opps....something went wrong");
     }
-    const users = await searchUserService(keyword);
+    const users = await searchUserService(keyword,userId);
     res.status(200).json(users);
   } catch (error) {
     next(error);
